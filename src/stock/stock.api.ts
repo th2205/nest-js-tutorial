f5c1ca115961c2d5c4ad6ef4fa92e34d2e11e0bd
th0566 <th05662205@gmail.com>
2021-01-28 23:31:27 +0900

    add api class

---
+++ src/stock/stock.api.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { ResponseDto } from './stock.dto';
+import { HTTPError } from '../lib/HTTPError';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class StockAPI {
+  constructor(private readonly config: ConfigService) {}
+  async fetchKospi() {
+    let result: { value: string; time: string }[];
+    let error: HTTPError | null;
+    console.log(this.config.get('STOCK_API_KEY'));
+    try {
+      const { data } = await axios.get(
+        `http://ecos.bok.or.kr/api/StatisticSearch/${this.config.get(
+          'STOCK_API_KEY',
+        )}/json/kr/1/50000/064Y001/DD/20040101/20201231/0001000`,
+      );
+
+      // if(data.inc)
+      const {
+        data: {
+          StatisticSearch: { row },
+        },
+      }: {
+        data: { StatisticSearch: { row: ResponseDto[] } };
+      } = await axios.get(
+        `http://ecos.bok.or.kr/api/StatisticSearch/${this.config.get(
+          'STOCK_API_KEY',
+        )}/json/kr/1/50000/064Y001/DD/20040101/20201231/0001000`,
+      );
+
+      result = row.map((data) => ({
+        value: data.DATA_VALUE,
+        time: data.TIME,
+      }));
+    } catch (e) {
+      error = new HTTPError(e.statusCode, e.message);
+    }
+
+    return { result, error };
+  }
+
+  async fetchKosdaq() {
+    let result: { value: string; time: string }[];
+    let error: HTTPError | null;
+
+    try {
+      const {
+        data: {
+          StatisticSearch: { row },
+        },
+      }: {
+        data: { StatisticSearch: { row: ResponseDto[] } };
+      } = await axios.get(
+        'http://ecos.bok.or.kr/api/StatisticSearch/7QMQRH93AK98G0IWSN0F/json/kr/1/50000/064Y001/DD/20040101/20201231/0089000',
+      );
+
+      result = row.map((data) => ({
+        value: data.DATA_VALUE,
+        time: data.TIME,
+      }));
+    } catch (e) {
+      error = new HTTPError(e.statusCode, e.message);
+    }
+
+    return { result, error };
+  }
+}
