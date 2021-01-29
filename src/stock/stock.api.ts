c20c76d3504adf996dc710260f4a4c7340822d62
th0566 <th05662205@gmail.com>
2021-01-29 22:21:09 +0900

    error handling

--- src/stock/stock.api.ts
+++ src/stock/stock.api.ts
@@ -9,34 +9,23 @@ export class StockAPI {
   constructor(private readonly config: ConfigService) {}
   async fetchKospi() {
     let result: { value: string; time: string }[];
-    let error: HTTPError | null;
-    console.log(this.config.get('STOCK_API_KEY'));
-    try {
-      const { data } = await axios.get(
-        `http://ecos.bok.or.kr/api/StatisticSearch/${this.config.get(
-          'STOCK_API_KEY',
-        )}/json/kr/1/50000/064Y001/DD/20040101/20201231/0001000`,
-      );
+    let error: HTTPError;
 
-      // if(data.inc)
-      const {
-        data: {
-          StatisticSearch: { row },
-        },
-      }: {
-        data: { StatisticSearch: { row: ResponseDto[] } };
-      } = await axios.get(
-        `http://ecos.bok.or.kr/api/StatisticSearch/${this.config.get(
-          'STOCK_API_KEY',
-        )}/json/kr/1/50000/064Y001/DD/20040101/20201231/0001000`,
-      );
+    const { data } = await axios.get(
+      `http://ecos.bok.or.kr/api/StatisticSearch/${this.config.get(
+        'STOCK_API_KEY',
+      )}/json/kr/1/50000/064Y001/DD/20040101/20201231/0001000`,
+    );
+
+    if (typeof data === 'string' && data.includes('인증')) {
+      error = new HTTPError(403, 'api key error');
+    } else {
+      const row: ResponseDto[] = data.StatisticSearch.row;
 
       result = row.map((data) => ({
         value: data.DATA_VALUE,
         time: data.TIME,
       }));
-    } catch (e) {
-      error = new HTTPError(e.statusCode, e.message);
     }
 
     return { result, error };
@@ -46,25 +35,21 @@ export class StockAPI {
     let result: { value: string; time: string }[];
     let error: HTTPError | null;
 
-    try {
-      const {
-        data: {
-          StatisticSearch: { row },
-        },
-      }: {
-        data: { StatisticSearch: { row: ResponseDto[] } };
-      } = await axios.get(
-        `http://ecos.bok.or.kr/api/StatisticSearch/${this.config.get(
-          'STOCK_API_KEY',
-        )}/json/kr/1/50000/064Y001/DD/20040101/20201231/0089000`,
-      );
+    const { data } = await axios.get(
+      `http://ecos.bok.or.kr/api/StatisticSearch/${this.config.get(
+        'STOCK_API_KEY',
+      )}/json/kr/1/50000/064Y001/DD/20040101/20201231/0089000`,
+    );
+
+    if (typeof data === 'string' && data.includes('인증')) {
+      error = new HTTPError(403, 'api key error');
+    } else {
+      const row: ResponseDto[] = data.StatisticSearch.row;
 
       result = row.map((data) => ({
         value: data.DATA_VALUE,
         time: data.TIME,
       }));
-    } catch (e) {
-      error = new HTTPError(e.statusCode, e.message);
     }
 
     return { result, error };
